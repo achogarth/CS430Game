2d347ea0253caeeae0d426ab774790fdc40ac591
gravelchi <achogarth@gmail.com>
2015-07-18 12:00:38 -0500

    working on location metho and added code to moveY method

--- common/Entity.cpp
+++ common/Entity.cpp
@@ -6,16 +6,13 @@
 #include <cstring>
 #include <common/objloader.hpp>
 
-using namespace std;
-using namespace glm;
+//using namespace glm;
 
 int position;
-int length;
+int vertexCount;
 int hitpoints;
 int speed;
 int currentTextureRow, currentTextureCol;
-
-
 
 Entity::Entity(
 	std::vector<glm::vec3> & vertexBuffer, 
@@ -26,17 +23,18 @@ Entity::Entity(
 	int textureRow,
 	int textureColumn)
 {
+	//save references to buffers
 	//save index of first vertex
 	position = int(vertexBuffer.size());
 	//read object file
 	loadOBJ(objPath, vertexBuffer, uvBuffer, normalBuffer);
 
 	//save number of vertices added to buffer
-	length = int(vertexBuffer.size()) - position;
+	vertexCount = int(vertexBuffer.size()) - position;
 
 	//set location of object
 	glm::mat4 translate = glm::translate(glm::mat4(1.0f),location);
-	for (int i = position; i < position + length; i++){
+	for (int i = position; i < position + vertexCount; i++){
 		glm::vec4 point = glm::vec4(vertexBuffer[i],1.0f);
 		point = translate * point;
 		vertexBuffer[i] = glm::vec3(point.x, point.y, point.z);
@@ -57,6 +55,7 @@ Entity::Entity(
 
 Entity::~Entity(void)
 {
+
 }
 
 //load the object from an object file
@@ -65,11 +64,15 @@ bool Entity::loadObject(const char * path,
 	std::vector<glm::vec2> & out_uvs,
 	std::vector<glm::vec3> & out_normals)
 {	
-
 	return loadOBJ(path, out_vertices, out_uvs, out_normals);
 }
 
-void Entity::collide()
+bool Entity::collide()
+{
+	return false;
+}
+
+void Entity::move()
 {
 }
 
@@ -80,15 +83,19 @@ int Entity::getBufferPosition()
 
 int Entity::getLengthInBuffer()
 {
-	return length;
+	return vertexCount;
 }
 
 void Entity::moveX()
 {
 }
 
-void Entity::moveY()
+void Entity::moveY(std::vector<glm::vec3> & vertexBuffer, float distance)
 {
+	for (int i = position; i < position + vertexCount; i++)
+	{
+		vertexBuffer[i].y += distance;
+	}
 }
 
 void Entity::setTexture(
@@ -112,4 +119,55 @@ void Entity::setTexture(
 	textureBuffer[position+4] = bottomLeft;//bottom left
 	textureBuffer[position+5] = bottomRight;//bottom right
 	
+}
+
+//having problems passing the vec4
+void getLocation(std::vector<glm::vec3> & vertexBuffer, glm::vec4 & point)
+{
+	//calculate center of entity and return the center point
+	
+	//calculate average x
+	float x, minX, maxX;
+	minX = maxX = vertexBuffer[position].x;
+
+	//calculate average y
+	float y, minY, maxY;
+	minY = maxY = vertexBuffer[position].y;
+	for (int i = position; i < (position + vertexCount); i++)
+	{
+		
+		//x values
+		float value = vertexBuffer[i].x;
+		if (value < minX)
+		{
+			minX = value;
+		}
+		else if (value > maxX)
+		{
+			maxX = value;
+		}
+		x = (minX + maxX)/2;
+
+		//y values
+		value = vertexBuffer[i].y;
+		if (value < minY)
+		{
+			minY = value;
+		}
+		else if (value > maxY)
+		{
+			maxY = value;
+		}
+
+		y = (minY + maxY)/2;
+		
+
+		
+	}
+
+	point.x = x;
+	point.y = y;
+	point.z = 0.0f;
+	point.w = 1.0f;
+
 }
--- common/Entity.h
+++ common/Entity.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <glm/glm.hpp>
 
+
 class Entity
 {
 public:
@@ -24,7 +25,9 @@ public:
 		std::vector<glm::vec2> & out_uvs,
 		std::vector<glm::vec3> & out_normals);
 
-	void collide();
+	virtual bool collide(); //virtual makes this abstract
+
+	virtual void move();
 
 	int getBufferPosition();
 
@@ -32,13 +35,16 @@ public:
 
 	void moveX();
 
-	void moveY();
+	void moveY(std::vector<glm::vec3> & vertexBuffer, float distance);
 
 	void setTexture(
 		int row, 
 		int col, 
 		std::vector<glm::vec2> & textureBuffer
 	);// integer between 0 and 7 for row, col to pick the
+
+	//this method has problems passing the vec4
+	void getLocation(std::vector<glm::vec3> & vertexBuffer, glm::vec4 & point);
 
 private:
 	int position;
