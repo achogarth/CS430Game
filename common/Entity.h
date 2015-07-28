bea51903134443c049d18b3b03ab6d06b4169f3a
gravelchi <achogarth@gmail.com>
2015-07-27 21:44:29 -0500

    level one in loop

--- common/Entity.h
+++ common/Entity.h
@@ -26,7 +26,7 @@ public:
 		std::vector<glm::vec2> & out_uvs,
 		std::vector<glm::vec3> & out_normals);
 
-	virtual bool collide(std::vector<glm::vec3> & vertexBuffer, std::vector<Entity*> & bullets, Entity * player, int enemyCount); //virtual makes this abstract
+	virtual bool collide(std::vector<glm::vec3> & vertexBuffer, std::vector<Entity*> & bullets, Entity * player, int &enemyCount); //virtual makes this abstract
 
 	virtual void move(std::vector<glm::vec3> & vertexBuffer, glm::vec3 location);
 
